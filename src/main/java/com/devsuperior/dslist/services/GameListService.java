44acead8f357e068866d2b1c9398f6d9179abdfe
Douglas Oliveira Batista <dobatista7@gmail.com>
2023-05-09 22:22:10 -0300

    Projection and findByList

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslist.dto.GameListDTO;
-import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.GameList;
 import com.devsuperior.dslist.repositories.GameListRepository;
 
@@ -16,6 +15,7 @@ public class GameListService {
 	
 	@Autowired
 	private GameListRepository gameListRepository;
+	
 	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll(){
